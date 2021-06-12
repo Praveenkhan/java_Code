befcf5bbb1cd466f609f51f5caecb1d70a63a0a4
Praveenkhan <85428959+Praveenkhan@users.noreply.github.com>
2021-06-12 16:10:41 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@
         public static void main(String args[]){  
          System.out.println("Hello Java");  
             System.out.println("Simple program");  
+            System.out.println("Simple program third");  
         }  
     }  
